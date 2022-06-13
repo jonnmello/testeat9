2d957d4d9005b14a97fced40c54effa075ca10f9
Jonatas Melo <jonnmello@gmail.com>
2022-06-13 18:33:04 -0300

    primeiro

---
+++ src/components/Navbar/index.tsx
@@ -0,0 +1,75 @@
+import './styles.css';
+import 'bootstrap/js/src/collapse.js';
+
+import { Link,  } from 'react-router-dom';
+import { useContext, useEffect } from 'react';
+import history from 'util/history';
+import { AuthContext } from 'AuthContext';
+import { getTokenData, isAuthenticated } from 'util/auth';
+import { removeAuthData } from 'util/storage';
+
+const Navbar = () => {
+  
+  const { authContextData, setAuthContextData } = useContext(AuthContext);
+
+  useEffect(() => {
+    if (isAuthenticated()) {
+      setAuthContextData({
+        authenticated: true,
+        tokenData: getTokenData(),
+      });
+    } else {
+      setAuthContextData({
+        authenticated: false,
+      });
+    }
+  }, [setAuthContextData]);
+
+  const handleLogoutClick = (event: React.MouseEvent<HTMLAnchorElement>) => {
+    event.preventDefault();
+    removeAuthData();
+    setAuthContextData({
+      authenticated: false,
+    });
+    history.replace('/');
+  };
+
+  return (
+    <nav className="navbar  bg-primary main-nav">
+      <div className="container-fluid">
+        <Link to="/" className="nav-logo-text">
+          <h4>MovieFlix</h4>
+        </Link>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#dscatalog-navbar"
+          aria-controls="dscatalog-navbar"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        
+        <div className="nav-login-logout">
+          {authContextData.authenticated ? (
+            <>
+              <span className="nav-username">
+                {' '}
+                {authContextData.tokenData?.user_name}
+              </span>
+              <a href="#logout" onClick={handleLogoutClick}>
+                LOGOUT
+              </a>
+            </>
+          ) : (
+            <Link to="/admin/auth">LOGIN</Link>
+          )}
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
---
+++ src/components/TestChildren/index.tsx
@@ -0,0 +1,19 @@
+
+type Props = {
+    price: number;
+    children: React.ReactNode;
+}
+
+const TestChildren = ({price, children} : Props)=> {
+
+    return (
+        <>
+         {children}
+        <h1>Preço = {price}</h1>
+        {children}
+        </>
+    );
+
+}
+
+export default TestChildren;
